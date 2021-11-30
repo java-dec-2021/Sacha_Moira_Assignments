64338705529ed3ca94291255564499378bae67a5
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-11-30 02:48:53 -0500

    Initial commit. Basic practice assignment...

---
+++ 01-java-fundamentals/01-Java First Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram{
+  public static void main(String[] args){
+    System.out.println("My name is Sacha Moira Rinn.");
+    System.out.println("I am 40 years old (or was it 30?)");
+    System.out.println("My hometown is nowhereville rural hellscape...");
+  }
+  }
