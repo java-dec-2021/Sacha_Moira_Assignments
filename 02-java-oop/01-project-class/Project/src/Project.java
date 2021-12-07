41f454fcfe00f077a0793adc49e12dd84b013135
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-12-06 20:18:35 -0500

    Finished Project Class practice assignment.'

---
+++ 02-java-oop/01-project-class/Project/src/Project.java
@@ -0,0 +1,20 @@
+public class Project {
+    private String name;
+    private String description;
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+}
---
+++ 02-java-oop/01-project-class/Project/src/ProjectTest.java
@@ -0,0 +1,10 @@
+public class ProjectTest {
+    public static void main(String[] args){
+        Project testProj = new Project();
+        testProj.setName("Sacha");
+        testProj.setDescription("A wicked bitch!");
+        System.out.println(testProj.getName());
+        System.out.println(testProj.getDescription());
+
+    }
+}
