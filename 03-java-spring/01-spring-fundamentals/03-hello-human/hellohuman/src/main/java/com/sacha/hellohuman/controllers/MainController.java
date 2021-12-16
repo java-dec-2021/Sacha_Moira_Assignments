330598f9d7110c42da6d0a8bb37aac53d47dafe1
Christopher Para <37555032+Ayllium-Rin@users.noreply.github.com>
2021-12-16 17:06:19 -0500

    Hello human core assignment completed.

---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/java/com/sacha/hellohuman/controllers/MainController.java
@@ -0,0 +1,23 @@
+package com.sacha.hellohuman.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String index(@RequestParam(value="name", required=false, defaultValue="Human") String name, 
+			@RequestParam(value="lName", required=false) String lName,
+			@RequestParam(value="num", required=false, defaultValue="1") int num,
+			Model firstName,
+			Model lastName,
+			Model toLoop) {
+		firstName.addAttribute("name", name);
+		lastName.addAttribute("lName", lName);
+		toLoop.addAttribute("num", num);
+		return "index.jsp";
+	}
+}
---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="css/styles.css" />
+<title>Insert title here</title>
+
+</head>
+<body>
+
+<c:forEach begin="1" end="${num}">
+<h1>Welcome <c:out value="${name}" /> <c:out value="${lName}"/>!!!</h1>
+</c:forEach>
+
+</body>
+</html>
